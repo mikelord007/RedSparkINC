63466642566bf3e1dd9a25c76e871cddc447dc62
agentmike007 <manujasan23@gmail.com>
2021-10-15 19:52:13 +0530

    modified Green Buton

--- client/src/components/GreenBtn/GreenBtn.js
+++ client/src/components/GreenBtn/GreenBtn.js
@@ -1,23 +1,14 @@
-import React,{useState,useRef} from "react";
-import { Redirect } from "react-router-dom";
+import React from "react";
 
 
 import "./style.css"
 
 const GreenBtn = ({content, id,to}) => {
 
-    const [redirect,setRedirect] = useState(false)
-    const updateRedirect = () => {
-        setRedirect(true)
-        buttonRef.current && buttonRef.current.focus();
-    }
-
-    const buttonRef = useRef(null)
 
     return(
         <>
-        <button id={id} key={to} className="greenbtn" onClick={updateRedirect}>{content}</button>
-        { redirect?<Redirect to={to}/>:null }
+        <button id={id} key={to} className="greenbtn">{content}</button>
         </>
     )
 }
