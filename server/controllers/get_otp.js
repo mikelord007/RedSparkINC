4cf1628e2c2d5f65c4013ead37e6db28bd8685ff
bennett1412 <bennettmadavana@gmail.com>
2022-02-04 00:54:43 +0530

    Added html template for emails

--- server/controllers/get_otp.js
+++ server/controllers/get_otp.js
@@ -1,53 +1,80 @@
 import nodemailer from "nodemailer";
+import handlebars from 'handlebars';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { logo } from "../templates/logo.js";
+
 
 export const sendOTP = async (req, res) => {
 
     // add email stuff here 
     // send otp to email
     const { email } = req.body;
-    const type = req.body.type?req.body.type:res.locals.type;
+    const type = req.body.type ? req.body.type : res.locals.type;
     try {
+
+        const __filename = fileURLToPath(import.meta.url);
+
+        const __dirname = path.dirname(__filename);
         let subject;
         let email_message;
+        let heading;
+
         if (!type) {
-            const response = { "Status": "Failure", "Details": "Type not provided", "erMsg":"Something went wrong, please try again" }
+            const response = { "Status": "Failure", "Details": "Type not provided", "erMsg": "Something went wrong, please try again" }
             console.log(response)
             return res.status(400).send(response)
         }
+        const emailTemplateSource = fs.readFileSync(path.join(__dirname, "../templates/index.hbs"), "utf8");
+
         console.log(type)
-        if (type){
-            if(type === 'VERIFICATION'){
+        if (type) {
+            if (type === 'VERIFICATION') {
                 subject = "Here's your otp";
-                email_message = `Thank you for signing up,OTP: ${res.locals.otp}`
+                email_message = `we're excited to have you get started.`;
+                heading = "Welcome!";
             }
-            if(type === 'RESET'){
+            if (type === 'RESET') {
                 subject = "OTP for reseting your password";
-                email_message = `Forgot your password? No worries, we got you.` + `\nYour OTP:${res.locals.otp}`;
+                email_message = `forgot your password? No worries, we got you.`;
+                heading = "Hey there!";
             }
-            if(type === 'VERIFICATION_PENDING'){
+            if (type === 'VERIFICATION_PENDING') {
                 subject = "OTP for verification";
-                email_message = `Your verification is pending. Here's the OTP: ${res.locals.otp}`;
+                email_message = `Your verification is pending.`;
+                heading = "Welcome!";
             }
         }
-            // Create nodemailer transporter
-            const transporter = nodemailer.createTransport({
-                host: 'smtp.gmail.com',
-                port: 587,
-                secure: false,
-                auth: {
-                    user: `${process.env.EMAIL}`,
-                    pass: `${process.env.PASS}`
-                },
-                tls: {
-                    ciphers: 'SSLv3'
-                }
-            });
+        const img_1 = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAC0lEQVR4XmNgAAIAAAUAAQYUdaMAAAAASUVORK5CYII=";
+
+        const template = handlebars.compile(emailTemplateSource);
+        const htmlToSend = template({
+            user: "RedSpark member",
+            message: email_message,
+            otp: res.locals.otp,
+            img_1:img_1,
+            logo:logo
+        });
+        // Create nodemailer transporter
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
+            auth: {
+                user: `${process.env.EMAIL}`,
+                pass: `${process.env.PASS}`
+            },
+            tls: {
+                ciphers: 'SSLv3'
+            }
+        });
 
         const mailOptions = {
             from: `"RedSparks Inc"<${process.env.EMAIL}>`,
             to: `${email}`,
             subject: subject,
-            text: email_message,
+            html: htmlToSend
         };
 
         transporter.verify(function (error, success) {
@@ -69,7 +96,7 @@ export const sendOTP = async (req, res) => {
         return res.status(200).json({ "Status": "Success", verification_key: res.locals.encoded.toString() });
     }
     catch (err) {
-        const response = { "Status": "Failure", "Details": err.message, "erMsg":"Something went wrong, please try again" }
+        const response = { "Status": "Failure", "Details": err.message, "erMsg": "Something went wrong, please try again" }
         return res.status(400).send(response)
     }
 }
