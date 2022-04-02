022ea7c1238208f745400cbf5bd9140485029f81
Bennett B Madavana <bennettmadavana@gmail.com>
2022-04-02 20:26:33 +0530

    Updated logo and mascot

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch,Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import Head from "./components/Head/Head"
 import UDchat from "./pages/UDchat/UDchat";
@@ -17,23 +17,59 @@ const App = () => {
     const mobile = window.innerWidth < 425
 
     if (mobile)
-    return (
-        <Router>
-            <Head />
-            <Switch>
-                <Route exact path='/' render={() => { if (localStorage.getItem('token')) return <Redirect to="/listings" />; else return <WelcomePage /> }} />
-                <Route exact path='/auth' render={() => { if (localStorage.getItem('token')) return <Redirect to="/listings" />; else return <AuthPage /> }} />
-                <Route exact path="/listings" render={() => { if (localStorage.getItem('token')) return <UDlistings/>; else return <AuthPage /> }} />
-                <Route exact path="/chat" render={() => { if (localStorage.getItem('token')) return <UDchat/>; else return <AuthPage /> }} />
-                <Route exact path="/trade" render={() => { if (localStorage.getItem('token')) return <UDtradehist/>; else return <AuthPage /> }} />
-                <Route exact path="/create" render={() => { if (localStorage.getItem('token')) return <UDcreatenew/>; else return <AuthPage /> }} />
-            </Switch>
-        </Router>
-    )
+        return (
+            <Router>
+                <Head />
+                <Switch>
+                    <Route exact path='/' render={
+                        () => {
+                            if (localStorage.getItem('token') !== null && (localStorage.getItem('token') !== undefined))
+                                return <Redirect to="/listings" />;
+                            else return <WelcomePage />;
+                        }
+                    } />
+                    <Route exact path='/auth' render={
+                        () => {
+                            if (localStorage.getItem('token') !== null && (localStorage.getItem('token') !== undefined))
+                                return <Redirect to="/listings" />;
+                            else return <AuthPage />;
+                        }
+                    } />
+                    <Route exact path="/listings" render={
+                        () => {
+                            if ((localStorage.getItem('token') !== null) && (localStorage.getItem('token') !== undefined))
+                                return <UDlistings />;
+                            else return <Redirect to="/auth" />;
+                        }
+                    } />
+                    <Route exact path="/chat" render={
+                        () => {
+                            if ((localStorage.getItem('token') !== null) && (localStorage.getItem('token') !== undefined))
+                                return <UDchat />;
+                            else return <Redirect to="/auth" />;
+                        }
+                    } />
+                    <Route exact path="/trade" render={
+                        () => {
+                            if ((localStorage.getItem('token') !== null) && (localStorage.getItem('token') !== undefined))
+                                return <UDtradehist />;
+                            else return <Redirect to="/auth" />;
+                        }
+                    } />
+                    <Route exact path="/create" render={
+                        () => {
+                            if ((localStorage.getItem('token') !== null) && (localStorage.getItem('token') !== undefined))
+                                return <UDcreatenew />;
+                            else return <Redirect to="/auth" />;
+                        }
+                    } />
+                </Switch>
+            </Router>
+        )
 
     else
         return (
-            <UndrConstruction/>
+            <UndrConstruction />
         )
 }
 
--- client/src/pages/WelcomePage/WelcomePage.js
+++ client/src/pages/WelcomePage/WelcomePage.js
@@ -3,7 +3,7 @@ import React,{useEffect} from 'react';
 
 import logoBig from './assets/logoBig.png';
 import red_curve from './assets/red_curve.svg';
-import mascot from './assets/mascot.svg';
+import mascot from './assets/mascot.png';
 import downArrow from './assets/downArrow.svg';
 import './WelcomePageStyle.css';
 import BackImg from '../../assets/bg_img.png'
